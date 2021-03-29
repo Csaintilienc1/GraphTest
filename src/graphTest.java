f1c0d8c657fc585b734a7bc479dbba26e4179dc4
Csaintilienc1 <saintilienclaudel@gmail.com>
2021-03-29 14:48:20 -0400

    Initial commit

---
+++ src/graphTest.java
@@ -0,0 +1,33 @@
+import org.w3c.dom.Node;
+
+import java.util.List;
+
+public class graphTest {
+
+    class GraphNode{
+        char c;
+        List adjList;
+
+    }
+
+         void dfs(GraphNode root){
+             if(root==null){
+                 return ;
+             }
+             print(root);
+
+             boolean marked=false;
+
+
+        }
+     List addMode(GraphNode root){
+
+
+        return
+}
+    private void print(GraphNode root) {
+        System.out.println(root.c);
+    }
+
+
+}
